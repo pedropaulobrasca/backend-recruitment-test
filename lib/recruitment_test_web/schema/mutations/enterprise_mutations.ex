220d07457b718c89a9c5d141e65fbfa44d2a73e5
Pedro Brasca <pedropaulobrasca@gmail.com>
2025-02-02 00:43:28 -0300

    feat: mutation para atualizar uma enterprise

--- lib/recruitment_test_web/schema/mutations/enterprise_mutations.ex
+++ lib/recruitment_test_web/schema/mutations/enterprise_mutations.ex
@@ -12,12 +12,32 @@ defmodule RecruitmentTestWeb.Schema.Mutations.EnterpriseMutations do
 
       resolve &create_enterprise/3
     end
+
+    field :update_enterprise, :enterprise do
+      arg :id, non_null(:id)
+      arg :name, :string
+      arg :commercial_name, :string
+      arg :cnpj, :string
+      arg :description, :string
+
+      resolve &update_enterprise/3
+    end
   end
 
   def create_enterprise(_parent, args, _resolution) do
     case Enterprises.create_enterprise(args) do
       {:ok, enterprise} -> {:ok, enterprise}
       {:error, changeset} -> {:error, "Erro ao criar empresa: #{inspect(changeset.errors)}"}
+    end
+  end
+
+  def update_enterprise(_parent, args, _resolution) do
+    enterprise = Enterprises.get_enterprise!(args.id)
+    IO.inspect(args)
+
+    case Enterprises.update_enterprise(enterprise, args) do
+      {:ok, enterprise} -> {:ok, enterprise}
+      {:error, changeset} -> {:error, "Erro ao atualizar empresa: #{inspect(changeset.errors)}"}
     end
   end
 end
